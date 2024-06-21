064b1ca102572ed20765b5bd501a10acc152d266
AleHernandez12 <167136495+AleHernandez12@users.noreply.github.com>
2024-06-21 14:00:49 -0600

    scrips
    Creacion de codigos de las suma

--- MyProyec/Assets/Scrips/Calculadora.cs
+++ MyProyec/Assets/Scrips/Calculadora.cs
@@ -1,23 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Calculadora : MonoBehaviour
 {
+    public TMP_InputField primerNumero;
+    public TMP_InputField SegundoNumero;
+    public TMP_Text resultado;
+
     //Variables 
 
 
 
     //Metodos 
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
 }
