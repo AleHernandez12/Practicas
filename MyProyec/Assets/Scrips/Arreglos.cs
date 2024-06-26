f06b751bab0ec9e8c5ffbc4b2d0b169835a2e4a3
AleHernandez12 <167136495+AleHernandez12@users.noreply.github.com>
2024-06-25 18:25:07 -0600

    Icio secion
    Arreglos codigos

--- MyProyec/Assets/Scrips/Arreglos.cs
+++ MyProyec/Assets/Scrips/Arreglos.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
+
 
 public class Arreglos : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject panelAlertas;
+    public string[] textoAlertas;
+
+
+    public List<string> usuarios =new List<string>();
+
+    public TMP_InputField nombreUsuario;
+    public TMP_Text alertasDisplay;
+    
     void Start()
     {
-        
+        usuarios.Add(item: "Gerry");
+        usuarios.Add(item: "Alan");
+        usuarios.Add(item: "Pepe");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AccederConUsuario()
     {
-        
+        if (usuarios.Contains(nombreUsuario.text))
+        {
+            panelAlertas.SetActive(true);
+            alertasDisplay.text = textoAlertas[0];
+        }
+        else
+        {
+            panelAlertas.SetActive(true);
+            alertasDisplay.text = textoAlertas[1];
+        }
     }
+
+    
 }
