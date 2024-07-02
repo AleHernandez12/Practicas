5ea9e0ad8673b746bf6eb2a236a029379ab17016
AleHernandez12 <167136495+AleHernandez12@users.noreply.github.com>
2024-07-02 09:12:41 -0600

    Completo
    Se completo el diseno de todos los aprtados y funcionamiento terminado

--- MyProyec/Assets/Scrips/FlavorController.cs
+++ MyProyec/Assets/Scrips/FlavorController.cs
@@ -6,38 +6,38 @@ using TMPro;
 
 public class ChangeImage : MonoBehaviour
 {
-    // Referencia al TMP_Dropdown
+    
     public TMP_Dropdown ColorOptions;
 
-    // Referencia al componente Image
+    
     public Image Pastel;
 
-    // Referencias a los sprites
+    
     public Sprite graySprite;
     public Sprite yellowSprite;
     public Sprite magentaSprite;
 
     private void Start()
     {
-        // Obtener el componente TMP_Dropdown si no se ha asignado
+        
         if (ColorOptions == null)
         {
             ColorOptions = GetComponent<TMP_Dropdown>();
         }
 
-        // Obtener el componente Image si no se ha asignado
+        
         if (Pastel == null)
         {
             Pastel = GameObject.Find("Pastel").GetComponent<Image>();
         }
 
-        // Asignar el método al evento onValueChanged
+        
         ColorOptions.onValueChanged.AddListener(delegate { ChangeFlavor(); });
     }
 
     public void ChangeFlavor()
     {
-        // Cambiar el sprite del componente Image basado en el valor del dropdown
+        
         switch (ColorOptions.value)
         {
             case 0:
@@ -53,7 +53,7 @@ public class ChangeImage : MonoBehaviour
                 break;
 
             default:
-                // Opcional: manejar casos adicionales o valores por defecto
+                
                 break;
         }
     }
